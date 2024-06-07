253284fad2f134620b8631d573cd85ee0b75e816
imran <imran012x@gmail.com>
2024-06-07 12:49:05 +0600

    Second Commit

--- app/src/main/java/com/unique/lab_4/MainActivity.java
+++ app/src/main/java/com/unique/lab_4/MainActivity.java
@@ -1,45 +1,51 @@
 package com.unique.lab_4;
 
-import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView animateButton = findViewById(R.id.imagebtn);
+        Button rotateButton = findViewById(R.id.rotateButton);
+        Button translateButton = findViewById(R.id.translateButton);
+        Button resizeButton = findViewById(R.id.resizeButton);
+        ImageView targetImage = findViewById(R.id.targetImage);
 
-        animateButton.setOnClickListener(new View.OnClickListener() {
+        rotateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Scale animation
-                ObjectAnimator scaleX = ObjectAnimator.ofFloat(v, "scaleX", 1f, 1.5f);
-                ObjectAnimator scaleY = ObjectAnimator.ofFloat(v, "scaleY", 1f, 1.5f);
+                ObjectAnimator rotate = ObjectAnimator.ofFloat(targetImage, "rotation", 0f, 360f);
+                rotate.setDuration(1000);
+                rotate.start();
+            }
+        });
 
-                // Rotate animation
-                ObjectAnimator rotate = ObjectAnimator.ofFloat(v, "rotation", 0f, 360f);
+        translateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ObjectAnimator translateX = ObjectAnimator.ofFloat(targetImage, "translationX", 0f, 300f);
+                translateX.setDuration(1000);
+                translateX.start();
+            }
+        });
 
-                // Translate animation
-                ObjectAnimator translateX = ObjectAnimator.ofFloat(v, "translationX", 0f, 300f);
-                ObjectAnimator translateY = ObjectAnimator.ofFloat(v, "translationY", 0f, 300f);
-
-                // AnimatorSet to play animations together
-                AnimatorSet animatorSet = new AnimatorSet();
-                animatorSet.playTogether(scaleX, scaleY, rotate, translateX, translateY);
-                animatorSet.setDuration(1000); // Animation duration in milliseconds
-                animatorSet.start();
+        resizeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ObjectAnimator scaleX = ObjectAnimator.ofFloat(targetImage, "scaleX", 1f, 1.5f);
+                ObjectAnimator scaleY = ObjectAnimator.ofFloat(targetImage, "scaleY", 1f, 1.5f);
+                scaleX.setDuration(1000);
+                scaleY.setDuration(1000);
+                scaleX.start();
+                scaleY.start();
             }
         });
     }
